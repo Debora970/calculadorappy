11eed93fc188de84fbf872346452e6bcf853ae3e
Debora970 <144387522+Debora970@users.noreply.github.com>
2023-09-12 17:53:58 -0400

    initial commit

--- script.js
+++ script.js
@@ -1,7 +1,46 @@
-let num1 = 1;
-let num2 = 2;
-let num3 = 3;
+const CALCULAR = document.getElementById('calcular');
+const ERROR = document.getElementById('error');
+const FLU = document.getElementById('flu');
+const MAN = document.getElementById('man');
 
-if(num1>num2){
-    console.log
-}
+CALCULAR.addEventListener('click', () => {
+    const DATO = document.getElementById('peso').value
+    console.log('dato ingresado: ', DATO)
+    if (DATO > 0 && DATO < 30){
+        ERROR.style.display = 'none'
+        let flujo = calHolliday(DATO);
+        let mantenimiento = flujo*1.5;
+        FLU.innerHTML = flujo + ' cc/hr';
+        MAN.innerHTML = 'm+m/2 ' + mantenimiento + ' cc/hr';
+        FLU.style.display = 'block';
+        MAN.style.display = 'block';
+    } else if(DATO > 30){
+        ERROR.style.display = 'none'
+        let supCorporal = (((peso * 4) + 7) / (peso + 90));
+        let mantenimiento = flujo*1.5;
+        FLU.innerHTML = 'm+m/2 ' + mantenimiento + ' cc/hr';
+        FLU.style.display = 'block';
+        MAN.style.display = 'block';
+    }
+    else{
+        ERROR.style.display = 'block';
+        FLU.style.display = 'none';
+        MAN.style.display = 'none';
+    }
+    function calHolliday(peso){
+        let resto = peso;
+        let flujo = 0;
+        if (resto>20){
+            let aux = resto-20;
+            flujo += aux*1;
+            resto -= aux;
+        } 
+        if (resto>10){
+            let aux = resto-10;
+            flujo += aux*2;
+            resto -= aux;
+        }
+        flujo += resto*4;
+        return flujo;
+    }
+})
